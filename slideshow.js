ea2ed5b207fbce3f129e79f78c69205d42f163d0
Ema Gomez <emajobs16606@gmail.com>
2025-05-06 08:59:14 -0400

    Add files via upload

--- slideshow.js
+++ slideshow.js
@@ -1,4 +1,4 @@
-let slideIndex = [1,1];
+let slideIndex = [1,1,1,1,1,1];
 let slideId = ["mySlides1", "mySlides2","mySlides3", "mySlides4","mySlides5", "mySlides6"]
 showSlides(1, 0);
 showSlides(1, 1);
@@ -15,10 +15,10 @@ function plusSlides(n, no) {
 function showSlides(n, no) {
   let i;
   let x = document.getElementsByClassName(slideId[no]);
-  if (n > x.length) {slideIndex[no] = 1}    
+  if (n > x.length) {slideIndex[no] = 1}
   if (n < 1) {slideIndex[no] = x.length}
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+     x[i].style.display = "none";
   }
-  x[slideIndex[no]-1].style.display = "block";  
+  x[slideIndex[no]-1].style.display = "block";
 }
